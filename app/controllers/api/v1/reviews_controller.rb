8cdc44272ffb62a38aed700486b5660e23ed895c
MasterPCFull <italocarvalho100@gmail.com>
2021-11-17 16:16:39 -0300

    new pages -pt2

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,34 @@
+module Api
+    module V1
+        Class ReviewsController < ApplicationController
+        
+            def create
+                review = Review.new(review_params)
+
+                if review.save
+                    render json: ReviewSerializer.new(review).serialized_json
+                    else
+                        render jsos: { error: review.erros.massages }, status: 422
+             end
+            end
+
+            def destroy
+                review = Review.find(params [:id])
+
+                if review.destroy
+                    head :no_content
+                    else
+                    render jsos: { error: review.erros.massages }, status: 422
+             end
+            end
+
+
+        private 
+
+        def preview_params
+            params.requere(:review).permit(:title, :description, :score, :airline_id)
+            
+   end
+  end
+ end
+end
